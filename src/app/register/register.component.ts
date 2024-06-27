aa76e0a861a1316da4a9bdb223ff83d81f1eb171
ClaudEbro <claude.ebrottie@gmail.com>
2024-06-27 22:16:29 +0000

    Reactive Forms

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from './user';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn, FormArray } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
+function ratingRangeValidator(min: number, max: number): ValidatorFn {
+   return (c: AbstractControl): { [key: string]: boolean } | any => { 
+    if (c.value != null && (isNaN(c.value) || c.value < min || c.value > max)){
+      return { rangeError: true};
+    }
+    return null;
+  };
+} 
+
+function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
+  
+  const emailControl = c.get('email');
+  const emailConfirmControl = c.get('confirmEmail');
+
+  if(emailControl?.pristine || emailConfirmControl?.pristine){
+    return null;
+  }
+
+  if(emailControl?.value === emailConfirmControl?.value){
+    return null;
+  }
+  return { match: true};
+}
 
 @Component({
   selector: 'app-register',
@@ -11,18 +35,101 @@ import { NgForm } from '@angular/forms';
 
 export class RegisterComponent implements OnInit{
   
-  
+  public registerForm!: FormGroup;
   public user: User = new User();
 
-  constructor() {}
+  public errorMsg!: string;
 
-  ngOnInit(): void {
+  private validationErrorsMessages: any = {
+    required: 'Enter your email !',
+    email: 'Your email is not valid.'
   }
 
-  public saveData(registerForm: NgForm) {
-    console.log(registerForm.form);
-    console.log('Values: ', JSON.stringify(registerForm.value));
+  constructor(private fb:FormBuilder ) {}
+
+  ngOnInit(): void {
+
+    this.registerForm = this.fb.group({
+      firstName:['', [Validators.required, Validators.maxLength(40)]],
+      lastName: ['', [Validators.required, Validators.minLength(4)]],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        confirmEmail: ['', [Validators.required]],
+      }, { validators: emailMatcher}),
+      phone: '',
+      rating: [null, ratingRangeValidator(1,5)],
+      notification: 'email',
+      sendCatalog: true,
+      addresses: this.fb.array([this.createAddressGroup()])
+    });
+
+    this.registerForm.get('notification')?.valueChanges.subscribe(value => {
+      this.setNotificationSetting(value)
+    });
+
+    const emailControl = this.registerForm.get('emailGroup.email');
+    emailControl?.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe(val => {
+      console.log(val);
+      this.setMessage(emailControl)
+    });
+  }
+
+
+  public get addressList(): FormArray {
+    return this.registerForm.get('addresses') as FormArray;
+  }
+
+  public addAddress():void {
+    this.addressList.push(this.createAddressGroup());
+  }
+
+  public saveData() {
+    console.log(this.registerForm);
+    console.log('Values: ', JSON.stringify(this.registerForm.value));
     console.log('Hello !');
+  }
+
+  public fillFormData(): void {
+    this.registerForm.patchValue({
+      firstName: 'John',
+      lastName: 'Doe',
+      sendCatalog: true
+    })
+  }
+
+  public setNotificationSetting(method: string): void {
+    const phoneControl = this.registerForm.get('phone');
+
+    if(method === 'text') {
+      phoneControl?.setValidators(Validators.required);
+    } else {
+      phoneControl?.clearValidators();
+    }
+
+    phoneControl?.updateValueAndValidity();
+  }
+
+  private createAddressGroup(): FormGroup {
+    return this.fb.group({
+      addressType: ['home'],
+      street1: [''],
+      street2: [''],
+      city: [''],
+      state: [''],
+      zip: [''],
+    })
+  }
+
+  private setMessage(val: AbstractControl): void {
+    this.errorMsg = '';
+
+    if((val.touched || val.dirty) && val.errors){
+      this.errorMsg = Object.keys(val.errors).map(
+        key => this.validationErrorsMessages[key]
+      ).join(' ');
+    } 
   }
 
 }
